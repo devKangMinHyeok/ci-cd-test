dc99d7819c945004e830e39ad1aa75dda553a913
Kang <rkdalsgur032@unist.ac.kr>
2023-01-10 11:27:32 +0900

    yml 설정 완료, eslint, prettier, jest 설정

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  arrowParens: "avoid",
+  endOfLine: "auto",
+  tabWidth: 2,
+};
