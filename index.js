3381bb25e08a076984d3654212de0a01569cb2d9
Kang <rkdalsgur032@unist.ac.kr>
2023-01-10 13:40:06 +0900

    add

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-export const sum = (a, b) => {
+export const add = (a, b) => {
   return a + b;
 };
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,5 @@
-import {sum} from '../index';
+import {add} from '../index';
 
 test('add 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(add(1, 2)).toBe(3);
 });
